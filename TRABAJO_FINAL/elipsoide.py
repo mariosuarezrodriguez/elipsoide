f3dc77cd1e2939b80efffb88ff3e884e0e1c3a41
MASTERTIG <MASTERTIG@192.168.247.127>
2020-12-26 12:49:14 +0100

    Version inicial de la práctica final.

---
+++ TRABAJO_FINAL/elipsoide.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+#Archivo: elipsoide.py
+#Autor: Mario Suárez Rodríguez, Carlos Conzalo Gil.
+#Descripción: desarrollo de una aplicación para el trabajo con elipsoides mediante
+#clases y objetos.
+
